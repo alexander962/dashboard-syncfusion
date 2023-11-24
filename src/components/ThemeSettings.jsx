0e254fbd03478566a66c7869803dab88be877ba3
aleksandrsamraev <alex.shamrayev@gmail.com>
2023-11-24 15:20:56 +0300

    add ColorPicker

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsCheck } from 'react-icons/bs';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { themeColors } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const ThemeSettings = () => {
-  return <div>ThemeSettings</div>;
+  return (
+    <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
+      <div></div>
+    </div>
+  );
 };
 
 export default ThemeSettings;
